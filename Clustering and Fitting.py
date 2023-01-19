892d471633f91deec3aaf4f74608f5d0f2d46c0c
as22agd <as22agd@herts.ac.uk>
2023-01-20 00:04:33 +0530

    Update Clustering and Fitting.py

--- Clustering and Fitting.py
+++ Clustering and Fitting.py
@@ -12,6 +12,10 @@ import wbgapi as wb
 from sklearn.cluster import KMeans
 import seaborn as sns
 import scipy.optimize as opt
+from scipy.optimize import curve_fit
+import itertools as iter
+
+
 
 # The list 'indicator' contains the required indicator IDs
 indicator = ["EN.ATM.CO2E.PC","EG.USE.ELEC.KH.PC"]
@@ -32,6 +36,26 @@ def norm_df(df):
     return df
 
 
+def fct(x, a, b, c):
+    return a*x**2+b*x+c
+
+# Function to calculate the error ranges
+def err_ranges(x, func, param, sigma):
+    lower = func(x, *param)
+    upper = lower
+    uplow = []
+    for p,s in zip(param, sigma):
+        pmin = p - s
+        pmax = p + s
+        uplow.append((pmin, pmax))
+    pmix = list(iter.product(*uplow))
+    for p in pmix:
+        y = func(x, *p)
+        lower = np.minimum(lower, y)
+        upper = np.maximum(upper, y)
+    return lower, upper
+
+
 # Reading data to dataframe using the function read_data(indicator,country_code)
 data= read_data(indicator, country_code)
 
@@ -42,7 +66,7 @@ data.index.names = ['Country', 'Year']
 data = data.reset_index()
 data.drop(['EG.USE.ELEC.KH.PC'], axis = 1, inplace = True)
 data["Year"] = pd.to_numeric(data["Year"])
-data = data.reset_index()
+
 
 # Normalised dataframe
 dt_norm = norm_df(data)
@@ -51,5 +75,37 @@ df_fit = dt_norm.drop('Country', axis = 1)
 k = KMeans(n_clusters=3, init='k-means++', random_state=0).fit(df_fit)
 # Plotting clusters of different countries based on CO2 emission
 sns.scatterplot(data=dt_norm, x="Country", y="EN.ATM.CO2E.PC", hue=k.labels_)
+plt.ylabel("CO2 Emission")
+plt.title("CO2 Emission Rate ")
 plt.legend()
 plt.show()
+
+
+# Dataframe containing the data of the country Australia
+data1 = data[(data['Country'] == 'AUS')]
+# Implementing curve_fit function
+val = data1.values
+x, y = val[:, 1], val[:, 2]
+prmet, cov = opt.curve_fit(fct, x, y)
+
+data1["pop_log"] = fct(x, *prmet)
+print("Parameters are: ", prmet)
+print("Covariance is: ", cov)
+plt.plot(x, data1["pop_log"], label="Fit")
+plt.plot(x, y, label="Data")
+plt.grid(True)
+plt.xlabel('Year')
+plt.ylabel('CO2 emissions')
+plt.title("CO2 emission rate in Australia")
+plt.legend(loc='best', fancybox=True, shadow=True)
+plt.show()
+
+# Extract the sigmas from the diagonal of the covariance matrix
+sigma = np.sqrt(np.diag(cov))
+print(sigma)
+low, up = err_ranges(x, fct, prmet, sigma)
+
+# Predicting the CO2 emission in next 10 years
+low, up = err_ranges(2030, fct, prmet, sigma)
+print("Forcasted CO2 emission in Australia 2030 ranges between", low, "and", up)
+
